a2a2c824783227bda2487d2f2d76007a3ae6c014
Jared-louky <jareds.louky@gmail.com>
2020-10-19 13:06:30 -0400

    first commit in new repo

--- Deliver_main.py
+++ Deliver_main.py
@@ -1,3 +1,14 @@
-##Test to commit, complications in the terminal 
+import sys, time 
 
 
+def print_slow(str):
+    for letter in str:
+        sys.stdout.write(letter)
+        sys.stdout.flush()
+        time.sleep(0.1)
+
+print("Hello, candidate. Welcome to CosaNostra Pizza Company, A 'Family' run 'corperate' conclomerant."
+      "We are now using a simluation to find the right individuals to fill roles in our delivery department")
+
+
+player_name = input("What is your name?  ")
